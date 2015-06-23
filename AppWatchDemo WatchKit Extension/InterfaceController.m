7165582b0d31c0c8206e0609c92e619c3bf840a1
zeze <18813155491@163.com>
2015-06-23 19:34:16 +0800

    WatchKit UseDemo

--- AppWatchDemo WatchKit Extension/InterfaceController.m
+++ AppWatchDemo WatchKit Extension/InterfaceController.m
@@ -13,22 +13,56 @@
 
 @property (weak, nonatomic) IBOutlet WKInterfaceTimer *tmier;
 
+@property (weak, nonatomic) IBOutlet WKInterfaceSwitch *mySwitch;
+
+@property (weak, nonatomic) IBOutlet WKInterfaceSlider *slider;
 
 @end
 
 
 @implementation InterfaceController
 
+- (IBAction)isStart:(BOOL)value {
+    
+    if(value == YES){
+        [self.mySwitch setTitle:@"停止计时"];
+        [self.tmier start];
+        return;
+        
+    }if(value == NO) {
+        [self.mySwitch setTitle:@"开始计时"];
+        [self.tmier stop];
+        
+        return;
+        
+    }
+    
+}
+
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
 
     // Configure interface objects here.
     NSLog(@"superView == %@",self.superclass);
+    [self.mySwitch setOn:YES];
+    [self.mySwitch setTitle:@"停止计时"];
     [self.tmier start];
+    
+    [NSTimer scheduledTimerWithTimeInterval:.2 target:self selector:@selector(heh) userInfo:nil repeats:YES];
     
 }
 
-
+- (void)heh
+{
+    static float i = 0;
+    i++;
+    if(i==10){
+        i=0;
+        [self.slider setValue:i];
+        return;
+    }
+    [self.slider setValue:i];
+}
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
     // as -viewWillApppear
